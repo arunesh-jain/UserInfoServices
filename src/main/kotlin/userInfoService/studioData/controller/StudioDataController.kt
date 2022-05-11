95caba404c705dfc89dbe7e84598a5804142e599
Uzairrr1 <mohd.uzair@knoldus.com>
2022-05-11 10:22:44 +0530

    adding put method

--- src/main/kotlin/userInfoService/studioData/controller/StudioDataController.kt
+++ src/main/kotlin/userInfoService/studioData/controller/StudioDataController.kt
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import userInfoService.studioData.entities.StudioEntities
 import userInfoService.studioData.repositories.StudioDetailsRepository
+import java.util.*
 
 @RestController
 class StudioDataController ( val repo : StudioDetailsRepository) {
@@ -16,6 +18,11 @@ class StudioDataController ( val repo : StudioDetailsRepository) {
     @PostMapping( "/save")
     fun addStudioDetails (@RequestBody studio : StudioEntities) : StudioEntities {
         return  repo.save(studio)
+    }
+    @GetMapping("/getOne/{id}")
+    fun getOneStudioDetails(@PathVariable id: String): Optional<StudioEntities>
+    {
+        return repo.findById(id)
     }
 
     @GetMapping ("/getAll")
@@ -27,5 +34,10 @@ class StudioDataController ( val repo : StudioDetailsRepository) {
     fun deleteStudioDetails (@PathVariable(value = "Id") Id: String) : HttpStatus {
         repo.deleteById(Id)
         return HttpStatus.MOVED_PERMANENTLY
+    }
+    @PutMapping
+    fun updateStudioDetails(@RequestBody studio: StudioEntities)
+    {
+        repo.save(studio)
     }
 }
